a48e6463818c7feee62aa36e87abd8093fa353b7
cybruGhost <stkibukah@gmail.com>
2026-02-21 04:01:58 +0300

    cachefix datapage.

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/settings/DataSettings.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/settings/DataSettings.kt
@@ -299,21 +299,10 @@ fun DataSettings() {
 
                         CacheSpaceIndicator(cacheType = CacheType.Images, horizontalPadding = 20.dp)
                         
-                        SettingsDescription(text = "${Formatter.formatShortFileSize(context, diskCacheSize)} ${stringResource(R.string.used)} (${diskCacheSize * 100 / coilDiskCacheMaxSize.bytes}%)")
-                         
-                        // Add diagnostic cache
-                         val cacheSize = me.knighthat.coil.ImageCacheFactory.getCacheSize()
-                         
-                         SettingsDescription(
-                             text = "${stringResource(R.string.cache_status)}: ${
-                                 if (cacheSize > 0) stringResource(R.string.cache_working) 
-                                 else stringResource(R.string.cache_empty)
-                            }"
-                        )
+                        SettingsDescription(text = "${Formatter.formatShortFileSize(context, diskCacheSize)} ${stringResource(R.string.used)} (${if (coilDiskCacheMaxSize.bytes > 0) "${diskCacheSize * 100 / coilDiskCacheMaxSize.bytes}%" else stringResource(R.string.unlimited)})")
 
                         }
                     }
-
                     binder?.cache?.let { cache ->
                         val diskCacheSize = remember(cache.cacheSpace, cleanCacheOfflineSongs) {
                             cache.cacheSpace
@@ -370,7 +359,7 @@ fun DataSettings() {
 
                         CacheSpaceIndicator(cacheType = CacheType.CachedSongs, horizontalPadding = 20.dp)
                         
-                        SettingsDescription(text = "${Formatter.formatShortFileSize(context, diskCacheSize)} ${stringResource(R.string.used)} (${diskCacheSize * 100 / exoPlayerDiskCacheMaxSize.bytes}%)")
+                         SettingsDescription(text = "${Formatter.formatShortFileSize(context, diskCacheSize)} ${stringResource(R.string.used)} (${if (exoPlayerDiskCacheMaxSize.bytes > 0) "${diskCacheSize * 100 / exoPlayerDiskCacheMaxSize.bytes}%" else stringResource(R.string.unlimited)})")
                         }
                     }
 
